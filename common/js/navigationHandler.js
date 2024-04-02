64cfc01ddc36793457a9db4ca3e10168b6c629ba
DeltaGamingCH <deltagaming.ch.nik@gmail.com>
2024-04-02 22:00:57 +0200

    Removed comments

--- common/js/navigationHandler.js
+++ common/js/navigationHandler.js
@@ -1,13 +1,11 @@
 document.querySelectorAll('.open-nav').forEach(function(button) {
     button.addEventListener('click', function() {
         document.querySelector('.nav').classList.toggle('open');
-        console.log('Request Open');
     });
 });
 
 document.querySelectorAll('.close-nav').forEach(function(link) {
     link.addEventListener('click', function() {
         document.querySelector('.nav').classList.remove('open');
-        console.log('Request Close');
     })
 })
