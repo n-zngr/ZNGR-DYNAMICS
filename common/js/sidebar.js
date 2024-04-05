0e4c527000b9e4cf5d8ad39373c334fd2f6e924e
DeltaGamingCH <deltagaming.ch.nik@gmail.com>
2024-04-05 18:01:33 +0200

    Attempted to fix sidebar (incomplete)

--- common/js/sidebar.js
+++ common/js/sidebar.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
         button.addEventListener('click', function() {
             document.querySelector('.nav').classList.add('open');
             document.querySelector('.sidebar').style.display = 'none';
+            document.querySelector('.sidebar').classList.add('open');
             console.log('Sidebar request open');
         });
     });
@@ -10,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.close-nav').forEach(function(link) {
         link.addEventListener('click', function() {
             document.querySelector('.nav').classList.remove('open');
+            document.querySelector('.sidebar').classList.remove('open');
             document.querySelector('.sidebar').style.display = '';
             console.log('Sidebar request close');
         });
