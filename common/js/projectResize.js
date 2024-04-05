1efcbcba6dfb7bd5fa46fc0e0c3cc292c8a7ad24
DeltaGamingCH <deltagaming.ch.nik@gmail.com>
2024-04-05 10:07:05 +0200

    Improved project card, added animation (incomplete)

--- common/js/projectResize.js
+++ common/js/projectResize.js
@@ -1,12 +1,28 @@
 const thumbnail = document.querySelector('.thumbnail');
 const description = document.querySelector('.description');
+let clicked = false;
 
-description.addEventListener('mouseenter', () => {
-    thumbnail.style.width = '40%';
-    description.style.width = '60%';
-});
-
-description.addEventListener('mouseleave', () => {
-    thumbnail.style.width = '';
-    description.style.width = '';
+description.addEventListener('click', () => {
+    if (!clicked) {
+        thumbnail.classList.add('thumbnail_hidden');
+        thumbnail.style.left = '-100%';
+        thumbnail.style.width = '0%';
+        description.style.width = '100%';
+        thumbnail.style.margin = '0';
+        setTimeout(() => {
+            thumbnail.style.padding = '0';
+        }, 100)
+        description.style.marginLeft = 'auto';
+    } else {
+        thumbnail.style.padding = '';
+        thumbnail.style.margin = '';
+        thumbnail.style.width = '';
+        description.style.width = '';
+        description.style.marginLeft = '';
+        thumbnail.style.position = '';
+        setTimeout(() => {
+            thumbnail.style.left = '';
+        }, 100);
+    }
+    clicked = !clicked;
 });
