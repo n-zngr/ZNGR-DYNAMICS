8cd77d75ead4f39a3b2f0d367026a3a279430212
DeltaGamingCH <deltagaming.ch.nik@gmail.com>
2024-04-06 12:25:49 +0200

    Connected nav to sidebar

--- common/js/sidebar.js
+++ common/js/sidebar.js
@@ -1,18 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.sidebar').forEach(function(button) {
         button.addEventListener('click', function() {
-            document.querySelector('.nav').classList.add('open');
-            document.querySelector('.sidebar').style.display = 'none';
-            document.querySelector('.sidebar').classList.add('open');
+            document.querySelector('.container-container').classList.add('open');
             console.log('Sidebar request open');
         });
     });
 
     document.querySelectorAll('.close-nav').forEach(function(link) {
         link.addEventListener('click', function() {
-            document.querySelector('.nav').classList.remove('open');
-            document.querySelector('.sidebar').classList.remove('open');
-            document.querySelector('.sidebar').style.display = '';
+            document.querySelector('.container-container').classList.remove('open');
             console.log('Sidebar request close');
         });
     });
