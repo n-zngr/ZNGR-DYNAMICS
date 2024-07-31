b0b26ae127d6165fd57a846fdc20af04f031b844
DeltaGamingCH <deltagaming.ch.nik@gmail.com>
2024-07-31 15:30:32 +0200

    Updated modal styling, minor scrolling behavior changes, Added list items dynamic generation

--- src/script/modal.js
+++ src/script/modal.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const modal = document.getElementById('modal');
         const modalTitle = document.getElementById('modal-title');
         const modalDescription = document.getElementById('modal-description');
+        const modalListContainer = document.querySelector('.modal-main-about-list');
         const closeBtn = document.querySelector('.close');
 
         projectCards.forEach(card => {
@@ -16,6 +17,18 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (project) {
                     modalTitle.textContent = project.title;
                     modalDescription.textContent = project.description;
+                    
+                    //Clearing initial list items
+                    modalListContainer.innerHTML = '';
+
+                    project.list.forEach(item => {
+                        const listItem = document.createElement('li');
+                        listItem.className = 'modal-main-about-list-item';
+                        listItem.textContent = item;
+                        modalListContainer.appendChild(listItem);
+                    });
+                    
+                    document.body.classList.add('modal-open'); //Disables body scrolling
                     modal.removeAttribute('style');
                     modal.classList.add('open');
                 }
@@ -24,14 +37,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
         closeBtn.addEventListener('click', () => {
             modal.classList.remove('open');
-            modal.style.visibility = 'hidden';
+            document.body.classList.remove('modal-open'); //Allows body scrolling
         });
 
         window.addEventListener('click', (event) => {
             if (event.target === modal) {
                 modal.classList.remove('open');
-                modal.style.visibility = 'hidden';
+                document.body.classList.remove('modal-open'); //Allows body scrolling
             }
         });
     });
 })
+
