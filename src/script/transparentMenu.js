d7290745c6b8e67391585ccec3d0bc8234d32818
DeltaGamingCH <deltagaming.ch.nik@gmail.com>
2024-08-21 18:32:00 +0200

    Updated nav about me, added buttons to nav hover effect, minor data changes

--- src/script/transparentMenu.js
+++ src/script/transparentMenu.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
         target.style.setProperty("--mouseY", `${y}px`);
     }
 
-    for(const navItem of document.querySelectorAll('.nav-main-item, .nav-main-holder-item')) {
+    for(const navItem of document.querySelectorAll('.nav-main-item, .nav-main-holder-item, .nav-main-item-button')) {
         navItem.onmousemove = e => mouseMove(e);
     }
 
